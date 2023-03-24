d1a0f3df674538198d45e216a07932a69543c1d5
TarasBlyznyuk <ipz222_btr@student.ztu.edu.ua>
2023-03-24 20:31:20 +0200

    Додано елементи для початку роботи зі сторінкою форми

---
+++ HelloName/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HelloName.Models;
+using HelloName.Controllers;
+
+namespace HelloName.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult KindaRequest() {
+            return View();
+        }
+    }
+}
