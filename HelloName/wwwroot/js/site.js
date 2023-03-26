b1b031566c1e81e5c2794a2a577cffbb3681028a
TarasBlyznyuk <ipz222_btr@student.ztu.edu.ua>
2023-03-26 14:18:57 +0300

    Робота виконана

--- HelloName/wwwroot/js/site.js
+++ HelloName/wwwroot/js/site.js
@@ -1,7 +1,2 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
-function showHiddenText() {
-    var hiddenText = document.getElementById("output");
-    hiddenText.style.display = "block";
-}
